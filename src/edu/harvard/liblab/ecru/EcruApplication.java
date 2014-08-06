0a042eaf70b9573efeae3bd85be99fccab1d6854
Fox, Bobbi <bobbi_fox@harvard.edu>
2014-08-06 13:44:41 -0400

    add licensing info

--- src/edu/harvard/liblab/ecru/EcruApplication.java
+++ src/edu/harvard/liblab/ecru/EcruApplication.java
@@ -2,7 +2,16 @@ package edu.harvard.liblab.ecru;
 
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
-
+/**********************************************************************
+ *   Please see LICENSE.txt
+ **********************************************************************/
+/**
+ * @author Bobbi Fox
+ *
+ *   Project:  ecru
+ *    Handles the registration of resource classes, etc.
+ *    
+*/
 public class EcruApplication extends ResourceConfig {
 
 	public EcruApplication() {
